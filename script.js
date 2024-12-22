1d351c96777d168b61d13b48ad4533974b4eb8ca
Arya Gupta <artagupta239gmail.com>
2024-12-23 02:56:27 +0530

    page is working perfectly

--- script.js
+++ script.js
@@ -4,21 +4,111 @@ clear = document.getElementById('clear');
 
 
 clear.addEventListener('click', () =>{
-    console.log("i'm here");
     let input_value = document.getElementById('input');
-    console.log(input_value.value);
     input_value.value = "";
+    dropdown = document.getElementById('dropdown');
+    dropdown.innerHTML = "";
+    dropdown.style.display = 'none';
 })
 
 search.addEventListener('click', () =>{
-    console.log('true');
-    fetch('travel_recommendation_api.json')
+    let input_value = document.getElementById('input').value.toLowerCase();
+    fetch('../travel_recommendation_api.json')
     .then(response => response.json())
     .then(data => {
+            console.log('data fetched');
 
+            dropdown = document.getElementById('dropdown');
+            dropdown.innerHTML = "";
+            dropdown.style.display = "block";
+            let content = "";
+
+            let display = data.countries.find(item => item.name.toLowerCase() === input_value);
+            console.log(display);
+
+            if(input_value === 'temples' || input_value == 'temple'){
+                display = data.temples;
+                console.log(display);
+            }
+            else if(input_value === 'beaches' || input_value === 'beach'){
+                display = data.beaches;
+                console.log(display);
+            }
+            if(display){
+                if(display.cities){
+                    display.cities.map(item => {
+                        content += `<div class = "component">
+                                        <h3>${item.name}</h3>
+                                        <img src=${item.imageUrl}></img>
+                                        <p>${item.description}</p>
+                                        <button>Visit</button>
+                                            </div>`
+                    })
+                }
+
+                display.forEach((item) => {
+                    content += `<div class = "component">
+                                    <h3>${item.name}</h3>
+                                    <img src=${item.imageUrl}></img>
+                                    <p>${item.description}</p>
+                                    <button>Visit</button>
+                                        </div>`
+                })
+                dropdown.innerHTML += content;
+            }
     })
 })
 
 magnify.addEventListener('click', () =>{
-    console.log('true');
+    let input_value = document.getElementById('input').value.toLowerCase();
+    fetch('../travel_recommendation_api.json')
+    .then(response => response.json())
+    .then(data => {
+            console.log('data fetched');
+
+            dropdown = document.getElementById('dropdown');
+            dropdown.innerHTML = "";
+            dropdown.style.display = "block";
+            let content = "";
+
+            let display = data.countries.find(item => item.name.toLowerCase() === input_value);
+            console.log(display);
+
+            if(input_value === 'temples' || input_value == 'temple'){
+                display = data.temples;
+                console.log(display);
+            }
+            else if(input_value === 'beaches' || input_value === 'beach'){
+                display = data.beaches;
+                console.log(display);
+            }
+            if(display){
+                if(display.cities){
+                    display.cities.map(item => {
+                        content += `<div class = "component">
+                                        <h3>${item.name}</h3>
+                                        <img src=${item.imageUrl}></img>
+                                        <p>${item.description}</p>
+                                        <button>Visit</button>
+                                            </div>`
+                    })
+                }
+
+                display.forEach((item) => {
+                    content += `<div class = "component">
+                                    <h3>${item.name}</h3>
+                                    <img src=${item.imageUrl}></img>
+                                    <p>${item.description}</p>
+                                    <button>Visit</button>
+                                        </div>`
+                })
+                dropdown.innerHTML += content;
+            }
+    })
+})
+
+document.body.addEventListener('click', () => {
+    dropdown = document.getElementById('dropdown');
+    dropdown.innerHTML = "";
+    dropdown.style.display = 'none';
 })
